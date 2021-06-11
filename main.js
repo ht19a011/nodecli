186a4b10092594fc43cc94c05990da59d2d4d224
ht19a011 <81752381+ht19a011@users.noreply.github.com>
2021-06-11 13:19:57 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//const { program } = require("commander");
+//const dddd=require("commander");
+//dddd.parse(process.argv);
+//const filePath = dddd.args[0];
+const program = require("commander");
+const fs = require("fs");
+const md2html=require("./md2html")
+
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
+program.parse(process.argv);
+const filePath = program.args[0];
+
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+    gfm:false,
+    ...program.opts(),
+};
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        process.exit(1);
+        return;
+    }
+
+    const html=md2html(file,cliOptions);
+    console.log(html);
+});
